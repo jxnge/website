6f0f4d2f9d924ee7c4e907f690f4511424690e9a
jxnge <filipmarczyk@gmail.com>
2023-12-07 13:19:49 +0100

    mobile nav changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
 function toggleMobileMenu(menu) {
   menu.classList.toggle('open');
 }
+
+function toggleMobileMenu(menu) {
+  menu.classList.toggle('open');
+  if (menu.classList.contains('open')) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = 'auto';
+  }
+}
